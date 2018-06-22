afdb425ab39a2db15799b967603efd41afddbd1d
Andrew Morrison <ammorrison99@yahoo.com>
2018-06-22 15:30:16 -0500

    Update for adjustable rate and annuity

--- npv.py
+++ npv.py
@@ -3,30 +3,32 @@ import numpy as np
 class NPV(object):
     """
     Creates a Net Present Value (NPV) object which keeps track of both a time and a value
-    The rate RATE is assumed to be on the appropiate scale for the time
-    The rate is assumned to be the same for every value
-    The default discount rate is set to .1
+    The rate is not assumned to be the same for every value
     The default tolerance TOL is set to a thousandth of a cent
 
     Setup
     -----
-    The discount rate for all instances can be set using NPV.RATE = <new_rate>
+    The discount rate for an instance can be set using NPV.set_rate(instance, new_rate)
     The tolerance can be changed using NPV.TOL = <new_tol>.
     
     Constructor Parameters
     ----------------------
     time (int) : an integer giving the time the value is view from
     value (float) : a dollar value
-    
+    rate (float) : a discount rate
+   
     Attributes
     ----------
     time (int) : an integer giving the time the value is view from
     value (float) : a dollar value
+    rate (float) : a discount rate
     
     Overload Methods
     ----------------
     X + Y : Adds two NPVs by converting them to the earliest time and then adding thier values
     X - Y : Subtracts one NPV from annother by converting them to the earliest time and then adding thier values
+    X + Y : Adds two NPVs with different rates by converting them to 0 time and adding their values
+    X - Y : Subtracts one NPV from another with a different rate by convering them to 0 time and adding their values
     a * X or X * a : multiplies an NPV X and a number a by multiplying X's value by a
     X / a : divides an NPV X by a number a by dividing X's value by a
     -X : set NPV X's value to its negative
@@ -37,36 +39,45 @@ class NPV(object):
     > : converts two NPVs them to a common time and checks if the left NPV's value is greater than the right NPV's value and not within tolerance
     < : converts two NPVs them to a common time and checks if the left NPV's value is greater than the right NPV's value and not within tolerance
     """
-    
-    # define global variables
-    global RATE
     global TOL
-    RATE = 0.1
     TOL = 10**-6
     
-    __slots__ = ["time", "value"]
-    def __init__(self, time, value):
+    __slots__ = ["time", "value", "rate", "periods", "cash_flow"]
+    def __init__(self, time, value, rate, periods=0, cash_flow=0):
         
         # check types
         if not isinstance(time, (int, np.integer)):
             raise TypeError("The time argument must be an int.")
         if not isinstance(value, (int, np.integer, float, np.float)):
             raise TypeError("The value argument must be a number.")
-        
+        if not isinstance(rate, (int, np.integer, float, np.float)):
+            raise TypeError("The rate argument must be a number.")
+
         # assign attributes
         self.time = time
         self.value = value
+        self.rate = rate
+        self.periods = periods
+        self.cash_flow = cash_flow
+
         return
     
     def __str__(self):
-        return "NPV(time = {time}, value = {value})".format(time = str(self.time), value = str(self.value))
+        return "NPV(time = {time}, value = {value}, rate = {rate})".format(time = str(self.time), value = str(self.value), rate = str(self.rate))
     
     def __repr__(self):
         return str(self)
+
+    def set_rate(self, input_rate):
+        if not isinstance(input_rate, (int, np.integer, float, np.float)):
+            raise TypeError("The rate argument must be a number.")
+
+        self.rate = input_rate
+        return self
     
     def shift_to(self, target_time):
         """
-        Converts a NPV to a taget time and its correct value at that time
+        Converts a NPV to a target time and its correct value at that time
         
         Arguments:
         ----------
@@ -74,23 +85,43 @@ class NPV(object):
         """
         
         # check type
+        # check for complete class
         if not isinstance(target_time, int):
-            raise TypeError("The traget_time argument must be an int.")
+            raise TypeError("The target_time argument must be an int.")            
         
-        new_value = self.value * (1 + RATE) ** (target_time - self.time)
+        new_value = self.value * (1 + self.rate) ** (target_time - self.time)
         new_time = target_time
-        new_NPV = NPV(new_time, new_value)
+        new_NPV = NPV(new_time, new_value, self.rate)
         return new_NPV
-        
+
+    def generate_annuity(self):
+        """
+        Adds a present value of an annuity to an original project cost
+
+        Arguments:
+        ----------
+        periods : How many times the payment will be collected/made
+        rate : rate of growth per period
+        cash_flow : How much money is payed per period
+        """
+
+        new_NPV = self.shift_to(0)
+        for x in range(1, self.periods + 1):
+            temp_NPV = NPV(x, self.cash_flow, self.rate).shift_to(0)
+            new_NPV.value += temp_NPV.value
+        return new_NPV
+
     def __add__(self, other):
         
         # check type
         if not isinstance(other, NPV):
             raise TypeError("Only NPVs can be added to NPVs.")
+        if (other.rate != self.rate):
+            raise TypeError("Only NPVs with the same rate can be added.")
             
         new_time = min(self.time, other.time)
         new_value = self.shift_to(new_time).value + other.shift_to(new_time).value
-        new_NPV = NPV(new_time, new_value)
+        new_NPV = NPV(new_time, new_value, self.rate)
         return new_NPV
         
     def __sub__(self, other):
@@ -98,18 +129,37 @@ class NPV(object):
         # check type
         if not isinstance(other, NPV):
             raise TypeError("Only NPVs can be subtracted from to NPVs")
-            
+        if (other.rate != self.rate):
+            raise TypeError("Only NPVs with the same rate can be subtracted.")
+
         new_time = min(self.time, other.time)
         new_value = self.shift_to(new_time).value - other.shift_to(new_time).value
-        new_NPV = NPV(new_time, new_value)
+        new_NPV = NPV(new_time, new_value, self.rate)
         return new_NPV
+
+    def force_add(self, other):
+        if (other.rate != self.rate):
+            new_value = self.shift_to(0).value + other.shift_to(0).value
+            new_NPV = NPV(0, new_value, 0)
+            return new_NPV
+        else:
+            raise TypeError("Force_add is only to be used for NPVs with different rates. Please use __add__")
+
+    def force_sub(self, other):
+        if (other.rate != self.rate):
+            new_value = self.shift_to(0).value - other.shift_to(0).value
+            new_NPV = NPV(0, new_value, 0)
+            return new_NPV
+        else:
+            raise TypeError("Force_sub is only to be used for NPVs with different rates. Please use __sub__")
+
     
     def __rmul__(self, other):
         
         # check type
         if not isinstance(other, (int, float)):
             raise TypeError("NPV can only be multiplied by numbers.")
-        return NPV(self.time, self.value * other)
+        return NPV(self.time, self.value * other, self.rate)
     
     __mul__ = __rmul__
     
@@ -119,16 +169,18 @@ class NPV(object):
         if not isinstance(other, (int, float)):
             raise TypeError("NPV can only be divided by numbers.")
             
-        return NPV(self.time, self.value / other)
+        return NPV(self.time, self.value / other, self.rate)
     
     def __neg__(self):
-        return NPV(self.time, -self.value)
+        return NPV(self.time, -self.value, self.rate)
     
     def __eq__(self, other):
         
         # check type
         if not isinstance(other, NPV):
             raise TypeError("Only NPVs can be compared.")
+        if (other.rate != self.rate):
+            raise TypeError("Only NPVs with the same rate can be compared.")    
             
         new_time = min(self.time, other.time)
         return abs(self.shift_to(new_time).value - other.shift_to(new_time).value) < TOL
@@ -138,6 +190,8 @@ class NPV(object):
         # check type
         if not isinstance(other, NPV):
             raise TypeError("Only NPVs can be compared.")
+        if (other.rate != self.rate):
+            raise TypeError("Only NPVs with the same rate can be compared.") 
             
         new_time = min(self.time, other.time)
         return self.shift_to(new_time).value < other.shift_to(new_time).value and self != other
@@ -147,6 +201,8 @@ class NPV(object):
         # check type
         if not isinstance(other, NPV):
             raise TypeError("Only NPVs can be compared.")
+        if (other.rate != self.rate):
+            raise TypeError("Only NPVs with the same rate can be compared.") 
             
         new_time = min(self.time, other.time)
         return self.shift_to(new_time).value > other.shift_to(new_time).value and self != other
@@ -156,6 +212,8 @@ class NPV(object):
         # check type
         if not isinstance(other, NPV):
             raise TypeError("Only NPVs can be compared.")
+        if (other.rate != self.rate):
+            raise TypeError("Only NPVs with the same rate can be compared.") 
             
         new_time = min(self.time, other.time)
         return self.shift_to(new_time).value < other.shift_to(new_time).value
@@ -165,7 +223,8 @@ class NPV(object):
         # check type
         if not isinstance(other, NPV):
             raise TypeError("Only NPVs can be compared.")
+        if (other.rate != self.rate):
+            raise TypeError("Only NPVs with the same rate can be compared.") 
             
         new_time = min(self.time, other.time)
         return self.shift_to(new_time).value > other.shift_to(new_time).value or self == other
-        
--- npv_unittest.py
+++ npv_unittest.py
@@ -1,105 +1,118 @@
-# test primes
 import unittest
 import numpy as np
 from npv import NPV
 
-# write the test first. The funtion will be called is_prime.
 class TestNPVClass(unittest.TestCase):
 
 	# test .shift_to() works
 	# test .shift_to() checks type
 	def test_shift_to(self):
-		self.assertAlmostEqual(NPV(1, 100).shift_to(0).value, 100 / 1.1)
-		self.assertAlmostEqual(NPV(0, 100).shift_to(1).value, 100 * 1.1)
-		self.assertRaises(TypeError, NPV(1, 100).shift_to, "str")
-		self.assertRaises(TypeError, NPV(1, 100).shift_to, 1.1)
+		self.assertAlmostEqual(NPV(1, 100, 0.1).shift_to(0).value, 100 / 1.1)
+		self.assertAlmostEqual(NPV(0, 100, 0.1).shift_to(1).value, 100 * 1.1)
+		self.assertRaises(TypeError, NPV(1, 100, 0.1).shift_to, "str")
+		self.assertRaises(TypeError, NPV(1, 100, 0.1).shift_to, 1.1)
+
+	def test_generate_annuity(self):
+		self.assertAlmostEqual(NPV(1, 0, 0.1, 10, 15).generate_annuity().value, 92.16850659)
 
 	# test == works
 	# test == checks types
 	def test_eq_comparison(self):
-		self.assertTrue(NPV(1, 100) == NPV(1, 100.00000000001))
-		self.assertFalse(NPV(1, 100) == NPV(0, 100))
-		self.assertRaises(TypeError, NPV(1,100).__eq__, "str")
-		self.assertRaises(TypeError, NPV(1,100).__eq__, 1)
+		self.assertTrue(NPV(1, 100, 0.1) == NPV(1, 100.00000000001, 0.1))
+		self.assertFalse(NPV(1, 100, 0.1) == NPV(0, 100, 0.1))
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__eq__, "str")
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__eq__, 1)
 
 	# test != works
 	# test != checks types
 	def test_ne_comparison(self):
-		self.assertFalse(NPV(1, 100) != NPV(1, 100.00000000001))
-		self.assertTrue(NPV(1, 100) != NPV(0, 100))
-		self.assertRaises(TypeError, NPV(1,100).__ne__, "str")
-		self.assertRaises(TypeError, NPV(1,100).__ne__, 1)
+		self.assertFalse(NPV(1, 100, 0.1) != NPV(1, 100.00000000001, 0.1))
+		self.assertTrue(NPV(1, 100, 0.1) != NPV(0, 100, 0.1))
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__ne__, "str")
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__ne__, 1)
 
 
 	# test <= works
 	# test <= checks types
 	def test_le_comparison(self):
-		self.assertTrue(NPV(1, 100) <= NPV(1, 100.00000000001))
-		self.assertTrue(NPV(1, 100) <= NPV(0, 100))
-		self.assertRaises(TypeError, NPV(1,100).__le__, "str")
-		self.assertRaises(TypeError, NPV(1,100).__le__, 1)
+		self.assertTrue(NPV(1, 100, 0.1) <= NPV(1, 100.00000000001, 0.1))
+		self.assertTrue(NPV(1, 100, 0.1) <= NPV(0, 100, 0.1))
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__le__, "str")
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__le__, 1)
 
 	# test < works
 	# test < checks types
 	def test_lt_comparison(self):
-		self.assertFalse(NPV(1, 100) < NPV(1, 100.00000000001))
-		self.assertTrue(NPV(1, 100) <= NPV(0, 100))
-		self.assertRaises(TypeError, NPV(1,100).__lt__, "str")
-		self.assertRaises(TypeError, NPV(1,100).__lt__, 1)
+		self.assertFalse(NPV(1, 100, 0.1) < NPV(1, 100.00000000001, 0.1))
+		self.assertTrue(NPV(1, 100, 0.1) <= NPV(0, 100, 0.1))
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__lt__, "str")
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__lt__, 1)
 
 
 	# test >= works
 	# test >= checks types
 	def test_ge_comparison(self):
-		self.assertTrue(NPV(1, 100) >= NPV(1, 100.00000000001))
-		self.assertFalse(NPV(1, 100) >= NPV(0, 100))
-		self.assertRaises(TypeError, NPV(1,100).__ge__, "str")
-		self.assertRaises(TypeError, NPV(1,100).__ge__, 1)
+		self.assertTrue(NPV(1, 100, 0.1) >= NPV(1, 100.00000000001, 0.1))
+		self.assertFalse(NPV(1, 100, 0.1) >= NPV(0, 100, 0.1))
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__ge__, "str")
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__ge__, 1)
 
 	# test > works
 	# test > checks types
 	def test_gt_comparison(self):
-		self.assertFalse(NPV(1, 100) > NPV(1, 100.00000000001))
-		self.assertFalse(NPV(1, 100) > NPV(0, 100))
-		self.assertRaises(TypeError, NPV(1,100).__gt__, "str")
-		self.assertRaises(TypeError, NPV(1,100).__gt__, 1)
+		self.assertFalse(NPV(1, 100, 0.1) > NPV(1, 100.00000000001, 0.1))
+		self.assertFalse(NPV(1, 100, 0.1) > NPV(0, 100, 0.1))
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__gt__, "str")
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__gt__, 1)
 
 	# test + works
 	# test + checks types
 	def test_add(self):
-		self.assertEqual(NPV(1, 100) + NPV(0, 100), NPV(0, 100 + 100 / 1.1))
-		self.assertRaises(TypeError, NPV(1,100).__add__, "str")
-		self.assertRaises(TypeError, NPV(1,100).__add__, 1)
+		self.assertEqual(NPV(1, 100, 0.1) + NPV(0, 100, 0.1), NPV(0, 100 + 100 / 1.1, 0.1))
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__add__, "str")
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__add__, 1)
 
 
 	# test - (sub and neg) works
 	# test - checks types
 	def test_sub(self):
-		self.assertEqual(NPV(1, 100) - NPV(0, 100), NPV(0, 100 / 1.1 - 100))
-		self.assertRaises(TypeError, NPV(1,100).__add__, "str")
-		self.assertRaises(TypeError, NPV(1,100).__add__, 1)
+		self.assertEqual(NPV(1, 100, 0.1) - NPV(0, 100, 0.1), NPV(0, 100 / 1.1 - 100, 0.1))
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__add__, "str")
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__add__, 1)
 
 	# test * works (rmul and mul)
 	# test * checks types
 	def test_mul(self):
-		self.assertEqual(NPV(1, 100) * 10, NPV(1, 100 * 10))
-		self.assertRaises(TypeError, NPV(1,100).__mul__, "str")
-		self.assertRaises(TypeError, NPV(1,100).__mul__, NPV(1, 100))
+		self.assertEqual(NPV(1, 100, 0.1) * 10, NPV(1, 100 * 10, 0.1))
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__mul__, "str")
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__mul__, NPV(1, 100, 0.1))
 
 	def test_rmul(self):
-		self.assertEqual(10 * NPV(1, 100), NPV(1, 100 * 10))
-		self.assertEqual(10 * NPV(1, 100), NPV(1, 100) * 10)
-		self.assertRaises(TypeError, NPV(1,100).__rmul__, "str")
-		self.assertRaises(TypeError, NPV(1,100).__rmul__, NPV(1, 100))
+		self.assertEqual(10 * NPV(1, 100, 0.1), NPV(1, 100 * 10, 0.1))
+		self.assertEqual(10 * NPV(1, 100, 0.1), NPV(1, 100, 0.1) * 10)
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__rmul__, "str")
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__rmul__, NPV(1, 100, 0.1))
 
 	# test / works
 	# test / checks types
 	# test division by zero
 	def test_truediv(self):
-		self.assertEqual(NPV(1, 100) / 10, NPV(1, 100) * (1/10))
-		self.assertRaises(TypeError, NPV(1,100).__truediv__, "str")
-		self.assertRaises(TypeError, NPV(1,100).__truediv__, NPV(1, 100))
-		self.assertRaises(ZeroDivisionError, NPV(1,1).__truediv__, 0)
+		self.assertEqual(NPV(1, 100, 0.1) / 10, NPV(1, 100, 0.1) * (1/10))
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__truediv__, "str")
+		self.assertRaises(TypeError, NPV(1,100, 0.1).__truediv__, NPV(1, 100, 0.1))
+		self.assertRaises(ZeroDivisionError, NPV(1,1, 0.1).__truediv__, 0)
+
+	def test_set_rate(self):
+		self.assertEqual(NPV(1, 100, 0).set_rate(0.1).rate , 0.1)
+		self.assertRaises(TypeError, NPV(1,100, 0.1).set_rate, "str")
+		
+	def test_force_add(self):
+		self.assertEqual(NPV(1, 100, 0.1).force_add(NPV(1, 100, 0.2)), NPV(0, 174.2424242424, 0))
+		self.assertRaises(TypeError, NPV(1, 100, 0.1).force_add, NPV(1, 200, 0.1))
+
+	def test_force_sub(self):
+		self.assertEqual(NPV(1, 100, 0.1).force_sub(NPV(1, 100, 0.2)), NPV(1, 7.57575757, 0))
+		self.assertRaises(TypeError, NPV(1, 100, 0.1).force_sub, NPV(1, 200, 0.1))
 
 if __name__ == "__main__":
-	unittest.main()
+   unittest.main()
